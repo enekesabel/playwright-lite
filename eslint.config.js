2590f2ff16088fbbaf4386649cb970c457151e16
enekesabel <enekes.abel@gmail.com>
2026-09-11 12:03:45 +0200

    chore: verify standalone build prerequisites

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,8 @@
-import config from "@ayme-dev/eslint-config/base";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default config;
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  { ignores: ["dist/**", "coverage/**", "playwright-report/**", "test-results/**"] },
+];
